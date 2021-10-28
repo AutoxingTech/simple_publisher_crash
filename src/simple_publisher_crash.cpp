f6cd18c10e0ff97539c7d2564a7b2ef63b3f6fa2
FengZhaolin <kingsimba0511@gmail.com>
2021-10-28 21:15:49 +0800

    try to avoid conflict by renaming

--- src/simple_publisher_crash.cpp
+++ src/simple_publisher_crash.cpp
@@ -1,32 +1,32 @@
 #include <ros/ros.h>
 #include "ros/console.h"
-#include "simple_publisher_crash/Alerts.h"
+#include "simple_publisher_crash/MyAlerts.h"
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "monitor_test");
+    ros::init(argc, argv, "simple_publisher_crash");
 
     ros::NodeHandle nh("~");
 
-    ros::Publisher alertsPub = nh.advertise<simple_publisher_crash::Alerts>("/alerts", 1, true);
+    ros::Publisher alertsPub = nh.advertise<simple_publisher_crash::MyAlerts>("/my_alerts", 1, true);
 
     ros::Rate loop(1);
     while (ros::ok())
     {
         ros::spinOnce();
 
-        simple_publisher_crash::Alerts result;
-        simple_publisher_crash::Alert alert;
-        // result->alerts.reserve(16);
+        simple_publisher_crash::MyAlerts msg;
+        simple_publisher_crash::MyAlert alert;
         alert.code = 1;
         alert.level = "";
         alert.msg = "";
-        result.alerts.push_back(alert);
+        msg.alerts.push_back(alert);
         alert.code = 2;
-        result.alerts.push_back(alert);
+        msg.alerts.push_back(alert);
         alert.code = 3;
-        result.alerts.push_back(alert);
-        alertsPub.publish(result);
+        msg.alerts.push_back(alert);
+
+        alertsPub.publish(msg);
 
         loop.sleep();
     }
