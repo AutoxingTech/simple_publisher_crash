b7da6570e2f8cb20fdcb7c607b94f0aa960e92c5
pangmj <mengjiepang@gmail.com>
2021-10-29 11:06:08 +0800

    gg

--- src/simple_publisher_crash.cpp
+++ src/simple_publisher_crash.cpp
@@ -20,10 +20,12 @@ int main(int argc, char** argv)
         simple_publisher_crash::MyAlert alert;
         alert.code = 1;
         alert.level = "";
-        for (int i = 0; i < 1000; i++)
+        alert.msg = "";
+        for (int i = 0; i < 10; i++)
         {
             alert.code = i;
             alert.level = std::to_string(i);
+            alert.msg = std::to_string(i) + "msg";
             msg.alerts.push_back(alert);
         }
         // alert.code = 2;
